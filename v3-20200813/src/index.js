a8ef7fe980ae2c832ce2d150656923d8d65d9363
john provencher <johnp5210@gmail.com>
2020-09-02 10:39:39 -0400

    jp edits

--- v3-20200813/src/index.js
+++ v3-20200813/src/index.js
@@ -200,3 +200,15 @@
 	        window.scrollTo(0, 0);
 	    }, 0);
 	}
+
+
+jQuery.fn.center = function () {
+    this.css("position", "fixed");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) +
+        $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
+        $(window).scrollLeft()) + "px");
+    return this;
+}
+
+//$('#apply').center();
